de0115660da4f6a6e75ccd2fbf87291d48c62b9f
git perforce import user <a@b>
2015-06-24 13:39:22 +0000

    Initial import of //depot/products/api/frontend/about-page/trunk/ from the state at revision #head
    [git-p4: depot-paths = "//depot/products/api/frontend/about-page/trunk/": change = 1231410]

---
+++ src/main/lib/about-page/js/about-page.js
@@ -0,0 +1,29 @@
+define([
+	'js-utils/js/base-page',
+    'text!about-page/templates/about-page.html',
+    'datatables',
+    'datatables-plugins/datatable-htmlsort'
+], function(BasePage, template) {
+
+	return BasePage.extend({
+		template: _.template(template),
+		
+		render: function() {
+			this.$el.html(this.template(this.getTemplateParameters()));
+
+            this.$('table.table').dataTable({
+                htmlFiltering: true,
+                bAutoWidth: false,
+                oLanguage: {
+                    sSearch: ''
+                }
+            });
+
+            this.$('.dataTables_filter input')
+                .prop('placeholder', this.getTemplateParameters().strings.search)
+                .addClass('search-query');
+		},
+
+        getTemplateParameters: $.noop
+	});
+});
